9366d940f9be2c543de556ef093c3239eee21d31
Carlos Vivas <carlosvivasb@gmail.com>
2020-10-07 19:43:52 -0400

    cambio en rama practica_5

--- practica_2.py
+++ practica_2.py
@@ -4,3 +4,7 @@ print("haciendo cambios en branch practica_3")
 print("ahora estoy haciendo cambios en la practica_4p")
 
 print("haciendo un cambio en el branch practica_4")
+
+print("ahora estoy haciendo la practica_5")
+
+print("haciendo un cambio en le branch practica_5")
