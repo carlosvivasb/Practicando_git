c07615e929e0e43ca8df4857d11331b8375a6fef
Carlos Vivas <carlosvivasb@gmail.com>
2020-10-12 20:30:33 -0400

    haciendo una modificacion en la rama practica_10

---
+++ practica_2.py
@@ -0,0 +1,22 @@
+
+print("haciendo cambios en branch practica_3")
+
+print("ahora estoy haciendo cambios en la practica_4p")
+
+print("haciendo un cambio en el branch practica_4")
+
+print("ahora estoy haciendo la practica_5")
+
+print("haciendo un cambio en el branch practica_5")
+
+print("hciendo un cambio en el branch practica_5 luego del pull resquest")
+
+print("ahora estoy haciendo la practica_6")
+
+print("empezando la practica haciendo cambios en el branch practica_7")
+
+print("haciendo una modificación en la rama practica_8")
+
+print("creando la rama practica_9")
+
+print("haciendo una modificacion en la rama practica_10")
